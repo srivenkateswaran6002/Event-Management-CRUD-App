40f921700941c33d37467178ced39e2c56d905d4
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-06 17:21:34 +0530

    Added loading to home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,15 +1,37 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import { fetchAllEvents } from "./api/api";
 import EventCard from "./components/EventCard"
 import NewEvent from "./components/NewEvent"
+import Loading from "./components/Loading";
 
-export default async function Home() {
+export default  function Home() {
   
-  let events = [];
-  events = await fetchAllEvents()
+  const [events , setEvents] = useState([])
+  const [loading , setLoading] = useState(false)
+  let sortedEvents = [];
+
+  useEffect(() => {
+    const getEvents = async () => {
+      try {
+        setLoading(true)
+        setEvents(await fetchAllEvents())      
+      }
+      catch(err) {
+        console.error("Error fetching events:", err)
+        setEvents([])
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+    getEvents()
+  } , [])
 
   // Sort the events only if list is not empty and by completed & upcoming order first
   if (events.length > 0) {
-    events.sort((a, b) => {
+    sortedEvents = [...events].sort((a, b) => {
       const now = new Date()
 
       const dateA = new Date(a.date)
@@ -22,22 +44,24 @@ export default async function Home() {
       if (!isCompletedA && isCompletedB) return -1
 
       return dateA - dateB
-    });
+    })
   }
 
   return (
+    loading ? (<Loading />) : (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4 text-center">Events</h1>
       {
-        (events.length === 0) ? (
+        (sortedEvents.length === 0) ? (
           <p>No events available.</p>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
-            {events.map((event) => <EventCard key={event.id} event={event} />)}
+            {sortedEvents.map((event) => <EventCard key={event.id} event={event} />)}
             <NewEvent />
           </div>
         )
       }
     </div>
-  );
+  )
+)
 }
