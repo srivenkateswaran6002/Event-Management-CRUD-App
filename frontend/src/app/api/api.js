eba09aa859d9535481cc2e2ebfc1570451455a10
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-06 15:50:46 +0530

    Completed making the event details page . CSS Needs some work.

--- frontend/src/app/api/api.js
+++ frontend/src/app/api/api.js
@@ -11,3 +11,13 @@ export async function fetchAllEvents() {
         throw err
     }
 }
+
+export async function fetchEventById(id) {
+    try {
+        const res = await axios.get(`${BASE_URL}/events/${id}/`)
+        return res.data
+    } catch (err) {
+        console.error(`Failed to fetch event with id ${id} from backend:`, err)
+        throw err
+    }
+}
