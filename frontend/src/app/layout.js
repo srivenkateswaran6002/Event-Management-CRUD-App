0a3fbe3c493c85b4d8d92b616224b171237e6fbb
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-06 17:59:09 +0530

    Update layout.js

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import "./globals.css"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">
+        <Link href="/">
         <header className="p-4 bg-zinc-900 text-white font-bold text-center">Event Management System</header>
+        </Link>
       {children}
       </body>
     </html>
