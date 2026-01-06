f14f59c3da93a4e419d8af37bdcbe2587f5bf0ae
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-06 19:15:14 +0530

    Updatedlayout.js to improve navbar

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,11 +3,16 @@ import "./globals.css"
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="bg-gray-100">
-        <Link href="/">
-        <header className="p-4 bg-zinc-900 text-white font-bold text-center">Event Management System</header>
+    <html lang="en" className="h-full">
+      <body className="h-full bg-gray-100">
+        <div className="w-full flex items-center justify-between bg-zinc-900 px-6 py-4">
+        <Link href="/" className="text-white font-bold text-xl">
+        Event Management System
         </Link>
+        <Link href="/events/new" className="bg-blue-600 hover:bg-blue-700 text-white font-bold text-center rounded px-4 py-2">
+        Create New Event
+        </Link>
+        </div>
       {children}
       </body>
     </html>
