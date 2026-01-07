6dc241a070478dfe68ad4f934e855a1f56a48505
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-07 20:13:49 +0530

    Minor Bug Fix on home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -46,7 +46,7 @@ export default async function Home({searchParams}) {
       <h1 className="text-2xl font-bold mb-4 text-center">Events</h1>
       {
         (events.length === 0) ? (
-          <p>No events available.</p>
+          <div className="content-center text-center align-middle"><p className="text-center text-4xl">No events available.</p></div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
             {events.map((event) => <EventCard key={event.id} event={event} />)}
